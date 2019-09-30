2659ae07b1ceb32b741f5faff014d02b94c1a6b5
Saulo Silva <saulo@s10design.com.br>
2019-09-30 12:34:43 -0300

    Resolução

--- src/books/dto/filter-book.dto.ts
+++ src/books/dto/filter-book.dto.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Matches } from 'class-validator';
 
 export class FilterBookDTO {
   @IsNotEmpty()
+  @Matches(/^\d{4}[\-\/\s]?((((0[13578])|(1[02]))[\-\/\s]?(([0-2][0-9])|(3[01])))|(((0[469])|(11))[\-\/\s]?(([0-2][0-9])|(30)))|(02[\-\/\s]?[0-2][0-9]))$/, {
+    message: 'Invalid date format. Format: YYYY-MM-DD'
+  })
   initialDate: string;
- 
+  
   @IsNotEmpty()
+  @Matches(/^\d{4}[\-\/\s]?((((0[13578])|(1[02]))[\-\/\s]?(([0-2][0-9])|(3[01])))|(((0[469])|(11))[\-\/\s]?(([0-2][0-9])|(30)))|(02[\-\/\s]?[0-2][0-9]))$/, {
+    message: 'Invalid date format. Format: YYYY-MM-DD'
+  })
   endDate: string;
 }
