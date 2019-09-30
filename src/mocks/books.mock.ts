536f88bf6ed4db63b6acb07d43920c82c97e823d
Gustavo Silva <xgustavossilva@gmail.com>
2019-09-29 22:05:49 -0300

    Update: Added date to mocks

--- src/mocks/books.mock.ts
+++ src/mocks/books.mock.ts
@@ -1,8 +1,8 @@
 export const BOOKS = [
-    { id: 1, title: 'First book', description: "This is the description for the first book", author: 'Olususi Oluyemi' },
-    { id: 2, title: 'Second book', description: "This is the description for the second book", author: 'John Barry' },
-    { id: 3, title: 'Third book', description: "This is the description for the third book", author: 'Clement Wilfred' },
-    { id: 4, title: 'Fourth book', description: "This is the description for the fourth book", author: 'Christian nwamba' },
-    { id: 5, title: 'Fifth book', description: "This is the description for the fifth book", author: 'Chris anderson' },
-    { id: 6, title: 'Sixth book', description: "This is the description for the sixth book", author: 'Olususi Oluyemi' },
+    { id: 1, title: 'First book', description: "This is the description for the first book", author: 'Olususi Oluyemi', date: '2019-01-31' },
+    { id: 2, title: 'Second book', description: "This is the description for the second book", author: 'John Barry', date: '2019-01-20' },
+    { id: 3, title: 'Third book', description: "This is the description for the third book", author: 'Clement Wilfred', date: '2019-02-05' },
+    { id: 4, title: 'Fourth book', description: "This is the description for the fourth book", author: 'Christian nwamba', date: '2019-05-27' },
+    { id: 5, title: 'Fifth book', description: "This is the description for the fifth book", author: 'Chris anderson', date: '2019-05-26' },
+    { id: 6, title: 'Sixth book', description: "This is the description for the sixth book", author: 'Olususi Oluyemi', date: '2019-05-01' },
 ];
